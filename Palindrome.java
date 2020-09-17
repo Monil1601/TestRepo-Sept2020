893d395ed213e4aed61442738e5d049bbb9e9418
Monil1601 <35006742+Monil1601@users.noreply.github.com>
2020-09-16 23:47:02 -0400

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+    static boolean check(String input) {
+        int i = 0;
+        int j = input.length() - 1;
+
+        while (i < j) {
+            //Compare the first character with the last character and return false if they are not equal
+            // String is not a palindrome if the program returns false
+            if (input.charAt(i) != input.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String");
+        String input = sc.next();
+
+        if (check(input))
+            System.out.println("String is a Palindrome");
+        else
+            System.out.println("Not a Palindrome");
+    }
+}
+
+
